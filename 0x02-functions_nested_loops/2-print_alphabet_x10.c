328f2a07c58e0b9bb1cd25e1160bf71041fbdf3c
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-02-25 14:34:09 -0800

    commit tasks

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print 10 times the alphabet in lower case
+*
+* Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			putchar(j);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
