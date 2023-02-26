bdd1a0d83cc2bc933506d42af70e36c42971b70d
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-02-26 02:51:54 -0800

    commit tasks

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+include "main.h"
+
+/**
+* print_sign - prints the sign of a number
+* @n: number to be checked
+*
+* Return: 1 and prints + if n is > 0
+* otherwise return 0 and prints 0 if n is zero
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+
+}
