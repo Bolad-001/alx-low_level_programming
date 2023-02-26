fcab67a0906f9c758536885013359981d25c2fd6
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-02-26 14:28:33 -0800

    commit tasks

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_last_digit - print last digit of a number
+* @n: last didgit number to be printed
+*
+* Return: the value of the last digit
+*/
+
+int print_last_digit(int n)
+{
+	int i = n % 10;
+
+	if (i < 0)
+	{
+		i *= -1;
+		putchar(i + '0');
+		return (i);
+	}
+}
