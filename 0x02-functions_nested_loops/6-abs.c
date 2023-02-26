f0f5fba0841d63826e1f04d28211fb111475b217
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-02-26 13:36:17 -0800

    committasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _abs - prints absolute value
+* @n: integer to n=be returned
+*
+* Return: absolute value of n
+*/
+
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
