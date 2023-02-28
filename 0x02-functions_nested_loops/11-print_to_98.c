daa55a73c3e43232e626a638243082c8b4973cad
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-02-28 08:40:51 -0800

    commit tasks

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print all natural numbers
+ * @n: statr from n
+ *
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = 0; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (n = 0; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
