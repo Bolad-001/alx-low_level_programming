eec37803e9e9c82c15875ae135c11f80e459f27a
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-03-01 07:17:24 -0800

    commit tasks

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+* _strlen - return the length of a string
+* @s: input string
+* Return: ALways 0
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	for (; s[len] != '\0'; len++)
+
+	return (len);
+}
