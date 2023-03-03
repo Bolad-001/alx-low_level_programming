6bcb940e52e77825d1b5fa22502a166fbe7fb5df
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-03-03 03:56:22 -0800

    commit tasks

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* rev_string - print a string in reverse
+* @s: input string
+* Return: Always 0
+*/
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	i++;
+
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[!]);
+		i--;
+	}
+}
