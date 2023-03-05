28e6b8c28ed449431f4bbe65a2ae5f971a7104cf
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-03-05 13:32:36 -0800

    commit tasks

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _isdigit - fuctiion that checks for a digit
+* @c: input checker
+*
+* Return: 1 if c is a digit otherwise 0
+*/
+
+int _isdigit(int c)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+		else
+			return (0);
+	}
+}
