dbd13dd8902684ae2d881c8fcd34ce32e6c72ae4
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-03-08 17:08:29 -0800

    commit tasks

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_square - function that prints a square in the terminal
+* followed by a new line
+* @size: input integer
+* Return: Always 0
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar(92);
+			_putchar('\n');
+		}
+	}
+		_putchar('\n');
+
+}
