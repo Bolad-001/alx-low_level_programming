443c2a5ddef338a109db6f98b45b33f092a98fe4
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-03-10 04:12:06 -0800

    commit task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_triangle - print a triangle followed by a new line
+* @size: input integer
+*
+* Return: Always 0
+*/
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
