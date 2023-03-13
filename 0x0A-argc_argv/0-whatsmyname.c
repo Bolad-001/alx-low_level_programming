a9943e75316c935b3f8eee30a9fc2ab79673a59d
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-03-13 09:55:36 -0700

    commit tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+* main - print name of the program
+* @argc: argument counter
+* @argv: argument vector
+* Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
