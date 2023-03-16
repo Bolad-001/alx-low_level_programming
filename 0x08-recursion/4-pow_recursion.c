bb98c71a2216d1a0c45638d6a73f3b78b80bc851
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-03-16 15:56:57 -0700

    commit tasks

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _pow_recursion - print the value of x
+* @x: input integer
+* @y: exponential
+* Return: -1 if y is lower than 0, otherwise returns x
+*/
+int _pow_recursion(int x, int y)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
