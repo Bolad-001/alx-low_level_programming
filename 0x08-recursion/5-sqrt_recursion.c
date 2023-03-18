9009c823878371728b160e02743e6c1e43363e7f
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-03-17 17:13:18 -0700

    commit tasaks

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _sqrt_recursion - returns the natural square root 
+* of a number
+* @n: input integer
+* Return: the natural square root of the number
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	else
+		return (square_root(1, n));
+}
+
+/**
+* square_root - find square root
+* @i: inpur value
+* @j: square root value
+* Return: the square root
+*/
+int square_root(int i, int j)
+{
+	if (i < j)
+		return (-1);
+	else if (i * i == j)
+		return (i);
+	return (square_root(i + 1, j));
+}
