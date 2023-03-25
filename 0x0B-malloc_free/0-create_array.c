58e1c8e152899dd46de135f073f722c8f3af9ab6
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-03-24 19:42:49 -0700

    Hot fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	s = malloc(sizeof(char) * size;
+	s = malloc(sizeof(char) * size)i;
 
 	if (s == NULL)
 		return (NULL);
