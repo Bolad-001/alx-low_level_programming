71067195753431dca9dd6b501c80caca7e831f8d
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-03-26 00:38:52 -0700

    commit task 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,9 +24,9 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	}
 
-	while (str[s1len] != '\0')
+	while (s1[s1len] != '\0')
 		s1len++;
-	while (str[s2len] != '\0')
+	while (s2[s2len] != '\0')
 		s2len++;
 
 	total_len = s1len + s2len;
