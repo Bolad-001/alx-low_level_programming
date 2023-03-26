158fdb4f9c68088efcd23de8afccacdbc2812d61
Adebayo Abubakar <adebayoabubakar1919@gmail.com>
2023-03-26 10:54:49 -0700

    hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	dimen_array = malloc(sizeof (int *) * height);
+	dimen_array = malloc(sizeof(int *) * height);
 
 	if (dimen_array == NULL)
 	{
