e51c9362a5927b29d7872b24400cce3849e8cc42
Bolad <adebayoabubakar1919@gmail.com>
2023-06-07 09:21:07 -0700

    commit task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function to return the
+ * length of a string recursively
+ * @s: string
+ * return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+}
