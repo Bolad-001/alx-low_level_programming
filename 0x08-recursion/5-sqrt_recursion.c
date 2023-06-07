a66c92d923d38a6255e1ce97bcbe483898631aac
Bolad <adebayoabubakar1919@gmail.com>
2023-06-07 16:44:52 -0700

    commit tak 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function to return the natural 
+ * square root of a number
+ * @n: integer to check
+ * Return: return the value recursively
+ */
+
+int _sqrt_recursion(int n)
+{
+	int guess_num = 1;
+
+	int find_sqrt (n, guess_num);
+
+	return (find_sqrt(int n, int guess_num));
+}
+
+/**
+ * find_sqrt - funtion to check for squreroot of a number
+ * @guess_num: natural int to check
+ * @n: integer to check
+ * Return: the natural value
+ */
+
+int find_sqrt(int n, int guess_num)
+{
+	if (n == guess_num * guess_num)
+	{
+		return (guess_num);
+	}
+	else if (guess_num < n)
+	{
+		return (find_sqrt(n, guess_num + 1))
+	}
+	else
+	{
+		return (-1);
+	}
+}
