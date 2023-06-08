8498441073a77c13f2fb382724530128eb0cf97a
Bolad <adebayoabubakar1919@gmail.com>
2023-06-08 15:13:18 -0700

    commit task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,28 +1,6 @@
 #include "main.h"
 
 /**
- * is_prime_number - function to check if an
- * input integer is a prime number
- * @n: integer to check
- * Return: 1 if integer is a prime number 
- * otherwise return 0
- */
-
-int is_prime_number(int n)
-{
-	int i = 2;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else
-	{
-		return (check_prime(n, i));
-	}
-}
-
-/**
  * check_prime - function that check for input integer greater
  * tha 2
  * @n: input integer
@@ -46,3 +24,26 @@ int check_prime(int n, int i)
 		return (check_prime(n, i + 1));
 	}
 }
+
+/**
+ * is_prime_number - function to check if an
+ * input integer is a prime number
+ * @n: integer to check
+ * Return: 1 if integer is a prime number 
+ * otherwise return 0
+ */
+
+int is_prime_number(int n)
+{
+	int i = 2;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (check_prime(n, i));
+	}
+}
+
