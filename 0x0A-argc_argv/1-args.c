0e468096e1007fba0a7c08b6431cb011111bf801
Bolad <adebayoabubakar1919@gmail.com>
2023-06-09 10:59:42 -0700

    commit task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the number of argument
+ * passed into it
+ * @argc: argument counter
+ * @argv: array of string passed
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc)
+
+	return (0);
+}
