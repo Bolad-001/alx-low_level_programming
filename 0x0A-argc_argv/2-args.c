3b211013fe42de91de028aeb83376c984e1948a9
Bolad <adebayoabubakar1919@gmail.com>
2023-06-09 15:02:32 -0700

    commit task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - funtion to print all arguments
+ * @argc: argument counter
+ * @argv: array of string to print
+ * @i: integer to iterate through the string
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if argc >= 0
+	{
+		printf("%s\n", argv[]);
+		
+		return (0);
+	}
+}
