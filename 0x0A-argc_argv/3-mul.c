58b0c3c3e3bffef4803ea6038fe13147616df4ab
Bolad <adebayoabubakar1919@gmail.com>
2023-06-09 16:15:43 -0700

    commit task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - function to print the result of multipli
+ * cation of two numbers
+ * @argc: argument counter
+ * @argv: array of strings to print
+ *
+ * Return: the product of two number otherwise error
+ * and return 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	result = atoi((argv[1]) * atoi(argv[2]));
+
+	printf("%d\n", result);
+
+	return (0);
+}
