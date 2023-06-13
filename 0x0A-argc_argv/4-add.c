a23076a70b53c554ff1b49cf3f8dfdcd6cea317d
Bolad <adebayoabubakar1919@gmail.com>
2023-06-13 09:50:13 -0700

    commit task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - function to add positive numbers
+ * @argc: argument counter
+ * @argv: arrays of strings
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, result = 0;
+
+	for (i = 1, i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result = result + atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
