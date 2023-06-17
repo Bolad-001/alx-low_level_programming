682d6050d0436f1b71936ebc499b9761fe4bfe49
Bolad <adebayoabubakar1919@gmail.com>
2023-06-17 09:22:46 -0700

    commit task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - function that allocate memory for 
+ * an array
+ * @nmeb: element of the array
+ * @size: size of the array
+ *
+ * Return:a pointer allocated to the array
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 ) || (size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
+}
