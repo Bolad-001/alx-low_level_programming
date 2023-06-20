89f9619b0dff5759cbd0096a5e6f56a443ab2235
Bolad <adebayoabubakar1919@gmail.com>
2023-06-20 12:37:21 -0700

    commit task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - function that initializes a variable
+ * struct dog
+ * @d: pointer to the struct
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
