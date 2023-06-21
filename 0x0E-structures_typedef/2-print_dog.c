06a05fbd1699f457bf79da3c20f856fe5e20e514
Bolad <adebayoabubakar1919@gmail.com>
2023-06-21 03:22:53 -0700

    hot fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,11 +19,11 @@ void print_dog(struct dog *d)
 
 	if ((*d).name == NULL)
 	{
-		printf("Name: (nil)\n");
+		printf("Name: (nil)");
 	}
 	if ((*d).owner == NULL)
 	{
-		printf("Owner: (nil)\n");
+		printf("Owner: (nil)");
 	}
 
 	printf("Name: %s\n", (*d).name);
