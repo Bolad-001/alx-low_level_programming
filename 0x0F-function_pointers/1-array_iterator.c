0075348efb79c9f0e60a86fcb1c474360b8a143e
Bolad <adebayoabubakar1919@gmail.com>
2023-06-22 01:51:34 -0700

    commit task 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
+
 /**
- * array_iterator: function that execute a funtion
+ * array_iterator - function that execute a funtion
  * given as a parameter
  * @size: size of the array
  * @action: function pointer
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches an integer
+ * @size: size of the array
+ * @array: array to check
+ * @cmp: function pointing to the array
+ *
+ * Return: -1 if size is <= 0 and if no element matches
+ * otherwise return the index of the first element
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (array != NULL || cmp != NULL)
+	{
+		return;
+	}
+
+	for (i = 0, i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+
+	}
+	return (-1);
+}
