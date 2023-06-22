9d3e2e7e50142262474100ead4cd1d0a5c7189cf
Bolad <adebayoabubakar1919@gmail.com>
2023-06-22 12:58:32 -0700

    commit 3-calc.h, 3-op_functions.c,3-main.c file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function to add
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the sum of two integr
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function to subtract two integer
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the difference of two integers
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function to multiply twi int
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the product of two int
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - funtion to divide two int
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of the divison
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function that return the remaider
+ * of two number
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: th remainder
+ */
+
+int op_mod(int a, int b)
+{
+	rteurn (a % b);
+}
