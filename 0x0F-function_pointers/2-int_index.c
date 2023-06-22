d4b399abd33f9d4869979f7857bb16fd44eedd2b
Bolad <adebayoabubakar1919@gmail.com>
2023-06-22 01:56:25 -0700

    commit task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -20,14 +20,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 	}
 	if (array != NULL || cmp != NULL)
 	{
-		return;
-	}
 
-	for (i = 0, i < size; i++)
-	{
-		if (cmp(array[i]))
-			return (i);
+		for (i = 0, i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
 
+		}
 	}
 	return (-1);
 }
