6a53ffc476331365df35f85a4661275c97a97f1a
Bolad <adebayoabubakar1919@gmail.com>
2023-06-22 14:02:44 -0700

    fix betty

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - function pointer that checks for the 
+ * get_op_func - function pointer that checks for the
  * correct funtion entegred by the user
  * @s: operator to check
  *
@@ -11,22 +11,23 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-    while (ops[i].op != NULL)
-    {
-    	if (strcmp(ops[i].op, s) == 0)
-		return (ops[i].f);
+	i = 0;
 
-	i++;
-    }
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
 
-    return (NULL);
+	return (NULL);
 }
