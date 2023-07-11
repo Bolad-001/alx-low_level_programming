eecf1479ac15a83156635a6f346a1ae45a1e7329
Bolad <adebayoabubakar1919@gmail.com>
2023-07-10 20:37:16 -0700

    commit task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,38 @@
 #include "main.h"
 
 /**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
+ * Return: 1 if it success. -1 if it fails.
  */
+int create_file(const char *filename, char *text_content)
+{
+	int fp;
+	int i;
+	int rw_File;
+
+	if (!filename)
+		return (-1);
+
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fp == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (i = 0; text_content[i]; i++)
+		;
+
+	rw_File = write(fp, text_content, i);
+
+	if (rw_File == -1)
+		return (-1);
+
+	close(fp);
+
+	return (1);
+}
