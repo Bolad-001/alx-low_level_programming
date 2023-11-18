3f8177844965118e0240da11dddc4e1dbe816641
Bolad <adebayoabubakar44@gmail.com>
2023-11-18 12:17:06 -0800

    commit task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,56 @@
 #include "dog.h"
 
+#include <stdlib.h>
+
+/**
+ * str_len - function that return the length of a string
+ * @s: string to check
+ *
+ * Return: the length of a string
+ */
+
+int str_len(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+
+}
+
+#include <stdlib.h>
+
+/**
+ * *str_cpy - function that copies a string
+ * @dest: pointer to buffer
+ * @src: string to copy
+ *
+ * Return: the pointer to string
+ */
+
+char *str_cpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
 /**
  * *new_dog - fucntion that create a new dog
  * @name: name of dog
