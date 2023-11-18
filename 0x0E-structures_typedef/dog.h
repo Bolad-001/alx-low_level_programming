9a7431c840cd38c7800eab9c7ee0f5eced88266b
Bolad <adebayoabubakar44@gmail.com>
2023-11-18 12:05:16 -0800

    fix error

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,7 +19,8 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-
+int str_len(char *s);
+char *str_cpy(char *dest, char *src);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
