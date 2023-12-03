7bcd548ca7c3f21ef6cd4fd817d5a2fb724d28a0
Bolad <adebayoabubakar44@gmail.com>
2023-12-03 12:36:15 -0800

    hot fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,16 +7,37 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = NULL;
+	/*
+	 *hash_table_t *table = NULL;
 
 	table = malloc(sizeof(hash_table_t));
-	if (!table)
+	if (table == NULL)
 	{
 		return (NULL);
 	}
 
 	table->array = malloc(sizeof(table->array) * size);
-	if (!table->array)
+	if (table == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	table->size = size;
+	return (table);
+	*/
+	hash_table_t *table = NULL;
+	hash_node_t **array;
+
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+
+	array = calloc(size, sizeof(hash_node_t *));
+	if (array == NULL)
 	{
 		free(table);
 		return (NULL);
